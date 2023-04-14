8e93e9813ff87e59424ff539b33b026b3da7c501
calibeach <edwardshei@gmail.com>
2023-04-14 14:44:44 -0700

    Hard coded api address

--- client/src/apis/api.ts
+++ client/src/apis/api.ts
@@ -1,6 +1,6 @@
 
   async function fetchSearchResults (searchQuery: string) {
-    const res = await fetch(`/api/search?q=${searchQuery}`);
+    const res = await fetch(`https://edwardshei-pulley-backend.onrender.com/api/search?q=${searchQuery}`);
     return await res.json();
   }
 
